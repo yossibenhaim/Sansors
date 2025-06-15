4175cf375470d94cf1026c3183b824f1a270630c
Yossi Ben Haim <v0585204770@gmail.com>
2025-06-15 15:40:00 +0300

    added the audio sensor

---
+++ Sensors/AudioSensor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensors
+{
+    internal class AudioSensor : ISensor
+    {
+        public string name { get; }
+
+        public bool active { get; private set; } = false;
+
+        public AudioSensor(string name)
+        {
+            this.name = name;
+        }
+        public void SetActive()
+        {
+            active = true;
+        } 
+    }
+}
--- Sensors/ISensor.cs
+++ Sensors/ISensor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,9 @@ namespace Sensors
 {
     internal interface ISensor
     {
-        string name { get; set; }
+        string name { get; }
 
-        bool active { get; set; }
+        bool active { get; }
 
     }
 }
