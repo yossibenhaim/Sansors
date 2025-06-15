80b03735f42efe91ae3512793a66d8074998f30a
Yossi Ben Haim <v0585204770@gmail.com>
2025-06-15 15:42:25 +0300

    added the thermal sensor

---
+++ Sensors/ThermalSensor .cs
@@ -0,0 +1,25 @@
+﻿using Sensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sansors
+{
+    internal class ThermalSensor : ISensor
+    {
+        public string name { get; }
+
+        public bool active { get; private set; } = false;
+
+        public ThermalSensor(string name)
+        {
+            this.name = name;
+        }
+        public void SetActive()
+        {
+            active = true;
+        }
+    }
+}
