828611b745600a1075d60cfa5a0894af0e86eb69
Yossi Ben Haim <v0585204770@gmail.com>
2025-06-17 11:30:12 +0300

    creat switch of chioce user of sensor

--- Sensors/UI/Dispaly.cs
+++ Sensors/UI/Dispaly.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 namespace Sensors
 {
     internal class Dispaly
-    { 
+    {
+        public void FlowGame()
+        {
+            bool run = true;
+
+            while (run)
+            {
+                string choice;
+                Console.WriteLine("enter chioce sensor");
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        break;
+                }
+                
+
+            }
+        }
     }
 }
